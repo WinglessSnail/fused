fbc309e0167206fd92e81347101d457f6198c0fd
winglesssnail <hashcalabell@gmail.com>
2024-09-11 09:25:44 +0330

    init final

---
+++ providers/ConvexClientProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import LoadingLogo from "@/components/shared/LoadingLogo";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+const CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL || "";
+const convexClient = new ConvexReactClient(CONVEX_URL);
+
+export const ConvexClientProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convexClient}>
+        <AuthLoading>
+          <LoadingLogo />
+        </AuthLoading>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
